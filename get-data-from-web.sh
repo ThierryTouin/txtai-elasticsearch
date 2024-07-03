9b4f07d193629e5be6b4b285e90aa6f85ce91916
Thierry Touin <thierrytouin.pro@gmail.com>
2024-07-03 14:13:54 +0200

    up to paraphrase-multilingual-MiniLM-L12-v2

---
+++ get-data-from-web.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd manageDatas
+echo start getMovies.js script
+node getMovies.js
--- load-data-in-elasticsearch.sh
+++ load-data-in-elasticsearch.sh
@@ -2,7 +2,5 @@
 cd manageDatas
 echo start createMoviesIndex.js script
 node createMoviesIndex.js
-echo start getMovies.js script
-node getMovies.js
 echo start putMovies.js script
 node putMovies.js
